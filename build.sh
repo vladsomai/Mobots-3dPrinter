c18757e41846a8f267689b17fd91a66b9a054fd8
Vlad Somai <somaivlad@gmail.com>
2023-07-15 21:46:05 +0300

    added group linking

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-C_COMPILER="/usr/bin/clang"
-CXX_COMPILER="/usr/bin/clang++"
+C_COMPILER="/usr/bin/clang-12"
+CXX_COMPILER="/usr/bin/clang++-12"
 FLAGS="-Werror -Wall -Wextra -Wc++17-extensions"
 cmake -G Ninja -D CMAKE_C_COMPILER=$C_COMPILER -D CMAKE_CXX_COMPILER=$CXX_COMPILER -D CMAKE_CXX_FLAGS_INIT=$FLAGS -S ./RoMoController -B ./out
